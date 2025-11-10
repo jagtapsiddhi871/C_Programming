8e788e4be28c1cdbcd833f945f39eb99e22f2d83
jagtapsiddhi871 <jagtapsiddhi1521@gmail.com>
2025-11-10 10:00:32 +0530

    Add files via upload

---
+++ Assignments/Assignment_15/program15_5.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int CountDiff(int iNo)
+{
+    int iEvenSum = 0;
+    int iOddSum = 0;
+    int iDigit = 0;
+    
+    while (iNo != 0)
+    {
+        
+        iDigit = iNo % 10;
+
+        if (iDigit % 2 == 0) 
+        {
+            iEvenSum = iEvenSum + iDigit;
+        }
+        else 
+        {
+            iOddSum = iOddSum + iDigit;
+        }
+
+        iNo = iNo / 10;
+    }
+   
+    return iEvenSum - iOddSum;
+
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number");
+    scanf("%d",&iValue);
+
+    iRet = CountDiff(iValue);
+
+    printf("%d",iRet);
+
+    return 0;
+}
