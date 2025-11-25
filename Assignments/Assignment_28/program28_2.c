facabca0c6bdb7c5b5bbc00889687b2b57c5ba0a
jagtapsiddhi871 <jagtapsiddhi1521@gmail.com>
2025-11-25 10:58:54 +0530

    Add files via upload

---
+++ Assignments/Assignment_28/program28_2.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+void Pattern(int iRow , int iCol)
+{
+    
+    int i =0, j = 0;
+
+    for( i= 1; i <= iRow; i++)
+    {
+        char cValue = 'A', cValue1 = 'a';
+
+        for(j = 1; j <= iCol; j++)
+        {
+            if((i % 2) != 0)
+            {
+                printf("%c\t",cValue);
+                cValue++;
+            }   
+            else
+            {
+                printf("%c\t",cValue1);
+                cValue1++;
+            }
+        } 
+      
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
